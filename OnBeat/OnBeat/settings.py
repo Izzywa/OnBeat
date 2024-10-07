eeb3a51b1a71a5b26d61d3f9272f3ba25194c77a
Izzatul Najwa <ismail.izzatulnajwa@gmail.com>
2024-10-07 14:55:40 +0800

    establish url with apps

--- OnBeat/OnBeat/settings.py
+++ OnBeat/OnBeat/settings.py
@@ -39,6 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'backend.apps.BackendConfig',
+    'frontend.apps.FrontendConfig',
+    'youtube.apps.YoutubeConfig'
 ]
 
 MIDDLEWARE = [
